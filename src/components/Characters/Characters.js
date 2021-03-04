5f06006dfc3cffac56b780487bc32d684ed4892c
Igor Pashkevich <igor.pashkevich@9y.co>
2021-03-04 11:00:59 +0300

    init-react-redux and init characters component

---
+++ src/components/Characters/Characters.js
@@ -0,0 +1,12 @@
+import { Card, Container } from '@material-ui/core'
+import React, { Component } from 'react'
+
+export default class Characters extends Component {
+    render() {
+        return (
+            <Container>
+                <h1>Rick and Morty Characters</h1>
+            </Container>
+        ) 
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import { render } from "react-dom"
+import { Provider } from "react-redux"
+import { applyMiddleware, compose, createStore } from "redux"
+import { loggerMiddleware } from "./middleware/logger"
+import { monitorReducerEnhancer } from "./enhancers/monitorReducer"
+import thunkMiddleware from "redux-thunk"
+import { rootReducer } from './reducers/index'
+import App from "./App"
 
-ReactDOM.render(
-  <React.StrictMode>
+const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunkMiddleware)
+const composeEnhancers = compose(middlewareEnhancer, monitorReducerEnhancer)
+const store = createStore(rootReducer, undefined, composeEnhancers)
+
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </Provider>,
+  document.getElementById("root")
+)
